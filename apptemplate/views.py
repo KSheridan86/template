ef20bec241d18b7a02f712ed50f06b8a41866093
Ken Sheridan <kensheridan86@gmail.com>
2022-08-18 18:21:26 +0000

    pre models addition commit

--- apptemplate/views.py
+++ apptemplate/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request):
+    msg = 'Welcome cunt face'
+    context = {
+        'msg': msg
+    }
+    return render(request, 'apptemplate/projects.html', context)
+
+def project(request, pk):
+    return render(request, 'apptemplate/single-project.html')
